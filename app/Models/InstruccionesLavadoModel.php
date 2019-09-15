ded50e09ae6d1ee8d3a125f81580f3f6d60939e1
Kevin Rodriguez Y <krodriguez1291@gmail.com>
2019-09-15 11:35:45 -0500

    commit inicial del proyecto

---
+++ app/Models/InstruccionesLavadoModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class InstruccionesLavadoModel extends Model
+{
+    //
+    protected $table = 'instrucciones_lavado';
+}
